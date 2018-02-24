ace4c31fa1d2709d565a33fa06dd46ee4d94f2bc
Derek Miller <derekjmiller@gmail.com>
2018-02-24 12:15:15 -0800

    Add project files.

---
+++ js/toggle.js
@@ -0,0 +1,16 @@
+﻿function toggleMenu() {
+    var elementToggle = document.getElementById("mainNav");
+    
+    if (elementToggle.className === "closed") {
+        elementToggle.className = "open";
+      
+  
+    } else {
+        elementToggle.className = "closed";
+        
+    }
+    console.log("click fired");
+}//toggleMenu()
+
+var toggleButton = document.getElementById("hamburgerBtn");
+toggleButton.onclick = toggleMenu;
